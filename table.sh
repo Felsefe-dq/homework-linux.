807639d41a56a2c77dafbe7cd6eb1ee809810105
Ubuntu <ubuntu@ip-172-31-20-30.ec2.internal>
2024-09-07 09:25:58 +0000

    modified file

--- table.sh
+++ table.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-
-for n  in {1..10};
+for n in {1..10};
 do
-	echo $n multiple by 5=$(($n*5)) 
+	echo 5 multiple by $n = $((5*$n))
+
+done	
+
+
+echo ---
+
+
+
+
+
+for n in {1..10};
+do
+        echo 6 multiple by $n = $((6*$n))
+
 done
 
+
+
+
+
+echo ---
+
+
+
+for n in {1..10};
+do
+        echo 7 multiple by $n = $((7*$n))
+
+done
 
